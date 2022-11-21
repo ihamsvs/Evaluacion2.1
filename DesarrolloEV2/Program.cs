407e3df47caa6e7623463b005b2e788dcf8c9229
kazerleth <matiasmirandav@gmail.com>
2022-11-20 22:10:12 -0300

    Test Commit: Cambio en opciones de menú

--- DesarrolloEV2/Program.cs
+++ DesarrolloEV2/Program.cs
@@ -18,11 +18,12 @@ namespace DesarrolloEV2
             while (!exit)
             {
                 Console.WriteLine("-------------- Bienvenido a Trueque --------------");
-                Console.WriteLine("1 - ¿ Desea intercambiar un objeto ? ");
+                Console.WriteLine("1 - ¿ Desea intercambiar un producto ? ");
                 Console.WriteLine("2 - ¿ Buscar un producto en la bodega ?");
                 Console.WriteLine("3 - ¿ Agregar un producto a la bodega ?");
                 Console.WriteLine("4 - Mostrar productos en bodega");
-                Console.WriteLine("5 - Salir de Trueque ");
+                Console.WriteLine("5 - Buscar trueques sin match");
+                Console.WriteLine("6 - Salir de Trueque ");
 
                 int opcion = int.Parse(Console.ReadLine());
 
@@ -32,22 +33,25 @@ namespace DesarrolloEV2
                         Console.WriteLine("Intercambiar producto");
                         break;
                     case 2:
-                        Console.WriteLine("Nombre del producto");
+                        Console.WriteLine("Nombre del producto:");
                         break;
                     case 3:
-                        Console.WriteLine("Que producto desea agregar");
-                        Console.WriteLine("Ingrese la llave del producto");
+                        Console.WriteLine("¿Qué producto desea agregar?");
+                        Console.WriteLine("Ingrese la llave del producto:");
                         int key = int.Parse(Console.ReadLine());
-                        Console.WriteLine("Ingrese el nombre del producto");
+                        Console.WriteLine("Ingrese el nombre del producto:");
                         string nombre = Console.ReadLine();
                         p.AgregarProducto(key, nombre);
                         break;
                     case 4:
-                        Console.WriteLine("Ingrese el identificador del producto");
+                        Console.WriteLine("Ingrese el identificador del producto:");
                         int llave = int.Parse(Console.ReadLine());
                         p.MostrarProductos(llave);
                         break;
                     case 5:
+                        Console.WriteLine("Buscando trueques sin match:");
+                        break;
+                    case 6:
                         Console.WriteLine("Cerrando aplicacion.........");
                         exit = true;
                         break;
